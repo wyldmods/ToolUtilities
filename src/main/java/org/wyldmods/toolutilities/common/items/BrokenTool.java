c66b6472366b682a2334fa2a142f77681ca23e61
Michael Lydeamore <michael.lydeamore@adelaide.edu.au>
2014-12-09 17:10:11 +1030

    Added some texturing, named information on tools

--- src/main/java/org/wyldmods/toolutilities/common/items/BrokenTool.java
+++ src/main/java/org/wyldmods/toolutilities/common/items/BrokenTool.java
@@ -1,12 +1,76 @@
 package org.wyldmods.toolutilities.common.items;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.IIcon;
+import net.minecraft.util.StatCollector;
 
 public class BrokenTool extends Item {
-	
+
 	public BrokenTool()
 	{
 		super();
+		this.setMaxStackSize(1);
+		this.setUnlocalizedName("brokenTool");
 	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public IIcon getIcon(ItemStack stack, int renderPass)
+	{
+		NBTTagCompound tag = stack.getTagCompound();
+		if (tag == null)
+			return (IIcon) null;
+		else
+		{
+			ItemStack oldStack = ItemStack.loadItemStackFromNBT(tag);
+			return oldStack.getItem().getIcon(oldStack, renderPass);
+		}
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public IIcon getIconIndex(ItemStack stack)
+	{
+		NBTTagCompound tag = stack.getTagCompound();
+		if (tag == null)
+			return (IIcon) null;
+		else
+		{
+			ItemStack oldStack = ItemStack.loadItemStackFromNBT(tag);
+			return oldStack.getItem().getIconIndex(oldStack);
+		}
+	}
+
+	@Override
+	public double getDurabilityForDisplay(ItemStack stack)
+	{
+		return 1;
+	}
+
+	@Override
+	public boolean showDurabilityBar(ItemStack stack)
+	{
+		return true;
+	}
+
+	@Override
+	public String getItemStackDisplayName(ItemStack stack)
+	{
+		NBTTagCompound tag = stack.getTagCompound();
+		if (tag == null)
+			return StatCollector.translateToLocal("brokentool.null");
+		else
+		{
+			ItemStack oldStack = ItemStack.loadItemStackFromNBT(tag);
+			System.out.println(oldStack.getDisplayName());
+			return StatCollector.translateToLocal("broken")+" "+StatCollector.translateToLocal(oldStack.getItem().getUnlocalizedName(oldStack)+".name");
+		}
+	}
+
 
 }
