a9891e4520be583d121070d5d81fe8cc1d316e72
mallrat208 <marwat208@gmail.com>
2015-03-14 04:34:32 -0400

    Match Existing Style

--- src/main/java/org/wyldmods/toolutilities/common/compat/RailcraftCompat.java
+++ src/main/java/org/wyldmods/toolutilities/common/compat/RailcraftCompat.java
@@ -1,9 +1,11 @@
 package org.wyldmods.toolutilities.common.compat;
 
+import static org.wyldmods.toolutilities.common.Config.swordAreaXP;
+import static org.wyldmods.toolutilities.common.Config.allowSwordAOE;
+
 import mods.railcraft.common.items.ItemCrowbar;
 
 import org.wyldmods.toolutilities.ToolUtilities;
-import org.wyldmods.toolutilities.common.Config;
 import org.wyldmods.toolutilities.common.recipe.ToolUpgrade;
 import org.wyldmods.toolutilities.common.recipe.ToolUpgradeRecipe;
 
@@ -11,6 +13,6 @@ public class RailcraftCompat {
 
     public static void addRailcraftRecipes()
     {
-        ToolUpgradeRecipe.addUpgradeRecipe(ItemCrowbar.class, ToolUtilities.swordAreaItem, ToolUpgrade.SWORD_AOE, Config.swordAreaXP, Config.allowSwordAOE);
+        ToolUpgradeRecipe.addUpgradeRecipe(ItemCrowbar.class, ToolUtilities.swordAreaItem, ToolUpgrade.SWORD_AOE, swordAreaXP, allowSwordAOE);
     }
 }
